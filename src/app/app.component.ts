6f895b7c81761fe01b167a2eb063c62d5e48ec6c
Joshua Loew <loewjc@gmail.com>
2020-06-17 14:04:19 -0400

    interactive map is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ElementRef } from '@angular/core';
+import { MapObject, VisitedCountries } from 'src/shared/map';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+
   title = 'world-map';
+  domParser;
+  pathObjects;
+
+  staticList = VisitedCountries;
+
+  constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef) { }
+
+  ngOnInit() {
+
+    this.domParser = new DOMParser();
+
+    const parsedHtml = this.domParser.parseFromString(MapObject.paths, "text/html");
+    const pathTags = parsedHtml.getElementsByTagName("path");
+    const elementsAsStrings = [];
+
+    Array.from(pathTags).forEach((element: any) => {
+
+      const hyphenatedCountry = element.title.toLowerCase().replace(/\ /g, '-');
+
+      if (this.staticList.includes(hyphenatedCountry)) {
+        element.className = "visited"
+        elementsAsStrings.push(`<a href="https://www.veronicaspann.com/${hyphenatedCountry}" target="_blank">${element.outerHTML}</a>`);
+      } else {
+        element.className = "not-visited"
+        elementsAsStrings.push(element.outerHTML);
+      }
+    
+    });
+    
+    this.pathObjects = this.sanitizer.bypassSecurityTrustHtml(elementsAsStrings.join());
+  
+  }
+
 }
