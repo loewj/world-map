61ca9b59148a9a6fba8e7fae171818bb8ddb981a
Joshua Loew <joshua.loew@nttdisruption.com>
2021-04-06 19:14:55 -0400

    improvements;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ElementRef } from '@angular/core';
+import {Component, ViewEncapsulation, ElementRef, OnInit, AfterViewInit} from '@angular/core';
 import { MapObject, VisitedCountries } from 'src/shared/map';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -10,7 +10,7 @@ declare const VERSION: string;
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
 
   title = 'Version: ' + VERSION;
   domParser;
@@ -25,49 +25,51 @@ export class AppComponent {
 
   ngOnInit() {
 
-    this.staticList.sort()
+    this.staticList.sort();
     this.hyperlinkedList = this.staticList.map(el => {
       return {
-        countryName: el.replace(/-/g, " "),
+        countryName: el.replace(/-/g, ' '),
         hyperlink: `https://www.veronicaspann.com/${el}`
-      }
-    })
+      };
+    });
 
     this.domParser = new DOMParser();
 
-    const parsedHtml = this.domParser.parseFromString(MapObject.paths, "text/html");
-    const pathTags = parsedHtml.getElementsByTagName("path");
+    const parsedHtml = this.domParser.parseFromString(MapObject.paths, 'text/html');
+    const pathTags = parsedHtml.getElementsByTagName('path');
     const elementsAsStrings = [];
 
     Array.from(pathTags).forEach((element: any) => {
 
-      const hyphenatedCountry = element.title.toLowerCase().replace(/\ /g, '-');
+      const hyphenatedCountry = element.title.toLowerCase().replace(/ /g, '-');
 
       if (this.staticList.includes(hyphenatedCountry)) {
-        element.className = "visited"
+        element.className = 'visited';
         elementsAsStrings.push(`<a href="https://www.veronicaspann.com/${hyphenatedCountry}" target="_blank">${element.outerHTML}</a>`);
       } else {
-        element.className = "not-visited"
+        element.className = 'not-visited';
         elementsAsStrings.push(element.outerHTML);
       }
-    
     });
-    
     this.pathObjects = this.sanitizer.bypassSecurityTrustHtml(elementsAsStrings.join());
-
   }
 
   ngAfterViewInit() {
     this.elementRef.nativeElement.querySelectorAll('.visited').forEach((el) => {
-      el.addEventListener("mouseenter", (event) => {   
+      el.addEventListener('mouseenter', (event) => {
         // highlight the mouseenter target
         this.hoverTitle = event.target.getAttribute('title');
-      })
-      el.addEventListener("mouseleave", (event) => {   
+      });
+      el.addEventListener('mouseleave', (event) => {
         // highlight the mouseenter target
         this.hoverTitle = '';
-      })
-    })
+      });
+    });
   }
- 
+
+  getRandomCountry() {
+    const country = this.staticList[Math.floor(Math.random() * this.staticList.length)];
+    window.open(`https://www.veronicaspann.com/${country}`, '_blank');
+  }
+
 }
